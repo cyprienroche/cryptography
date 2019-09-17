fd4a3c9df8bc972355f997a42a2add611c7cd146
cr618 <cyprien.roche18@imperial.ac.uk>
2019-09-17 16:28:39 +0100

    implemented helper functions for public encryption

--- Crypto.hs
+++ Crypto.hs
@@ -4,50 +4,68 @@ import Data.Char
 
 import Prelude hiding (gcd)
 
-{- 
-The advantage of symmetric encryption schemes like AES is that they are efficient 
-and we can encrypt data of arbitrary size. The problem is how to share the key. 
-The flaw of the RSA is that it is slow and we can only encrypt data of size lower 
+{-
+The advantage of symmetric encryption schemes like AES is that they are efficient
+and we can encrypt data of arbitrary size. The problem is how to share the key.
+The flaw of the RSA is that it is slow and we can only encrypt data of size lower
 than the RSA modulus n, usually around 1024 bits (64 bits for this exercise!).
 
-We usually encrypt messages with a private encryption scheme like AES-256 with 
-a symmetric key k. The key k of fixed size 256 bits for example is then exchanged 
-via the aymmetric RSA. 
+We usually encrypt messages with a private encryption scheme like AES-256 with
+a symmetric key k. The key k of fixed size 256 bits for example is then exchanged
+via the aymmetric RSA.
 -}
 
 -------------------------------------------------------------------------------
 -- PART 1 : asymmetric encryption
 
 gcd :: Int -> Int -> Int
-gcd m n = error "TODO: implement gcd"
+gcd m 0 = m
+gcd m n = gcd n (mod m n)
 
 phi :: Int -> Int
-phi m = error "TODO: implement phi"
+phi m = length [x | x <- [1 .. m], gcd x m == 1]
 
 --
--- Calculates (u, v, d) the gcd (d) and Bezout coefficients (u and v) 
+-- Calculates (u, v, d) the gcd (d) and Bezout coefficients (u and v)
 -- such that au + bv = d
 --
 extendedGCD :: Int -> Int -> ((Int, Int), Int)
-extendedGCD a b = error "TODO: implement extendedGCD"
+extendedGCD a 0 = ((1, 0), a)
+extendedGCD a b = ((v', (u' - q * v')), d)
+  where
+    ((u', v'), d) = extendedGCD b r
+    (q, r) = quotRem a b
+
 
 -- Inverse of a modulo m
 inverse :: Int -> Int -> Int
-inverse a m = error "TODO: implement inverse"
+inverse a m
+  | g == 1 = mod u m
+    where
+      ((u, v), g) = extendedGCD a m
 
 -- Calculates (a^k mod m)
--- 
+--
 modPow :: Int -> Int -> Int -> Int
-modPow a k m = error "TODO: implement modPow"
+modPow a 0 m = mod 1 m
+modPow a k m
+  | even k    = modPow b (div k 2) m
+  | otherwise = mod (a * modPow b (div (k - 1) 2) m) m
+  where
+    b = (mod (a * a) m)
 
 -- Returns the smallest integer that is coprime with phi
 smallestCoPrimeOf :: Int -> Int
-smallestCoPrimeOf phi = error "TODO: implement smallestCoPrimeOf"
+smallestCoPrimeOf phi = head [x | x <- [2 ..], gcd phi x == 1]
 
 -- Generates keys pairs (public, private) = ((e, n), (d, n))
 -- given two "large" distinct primes, p and q
 genKeys :: Int -> Int -> ((Int, Int), (Int, Int))
-genKeys p q = error "TODO: implement genKeys"
+genKeys p q =((e, n), (d, n))
+  where
+    d = inverse e ((p - 1) * (q - 1))
+    n = p * q
+    e = smallestCoPrimeOf ((p - 1) * (q - 1))
 
 -- RSA encryption/decryption; (e, n) is the public key
 rsaEncrypt :: Int -> (Int, Int) -> Int
